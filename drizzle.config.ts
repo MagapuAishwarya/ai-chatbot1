edb35b52d2f7fe51f34a9df2a82361625c2723cf
Magapu Girija Venkata Aishwarya <girijamagapu123@gmail.com>
2026-08-12 02:24:11 +0530

    Add files via upload

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "drizzle-kit";
+import "dotenv/config";
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  migrations: {
+    schema: "drizzle",
+    table: "__drizzle_migrations",
+  },
+});
