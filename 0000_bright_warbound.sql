21fa2686c6b503b652b7c801cc75f0cbf2385c1e
Magapu Girija Venkata Aishwarya <girijamagapu123@gmail.com>
2026-08-12 02:33:58 +0530

    Delete 0000_bright_warbound.sql

--- 0000_bright_warbound.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "messages" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"session_id" varchar(100) NOT NULL,
-	"role" varchar(100) NOT NULL,
-	"message" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL
-);
